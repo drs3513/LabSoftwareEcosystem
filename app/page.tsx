91b2d657fb4813fa959102aab93aed1ef23c5903
drs3513 <drsteven@uark.edu>
2025-01-28 12:50:40 -0600

    Auth added *QUICKSTART ENDED*

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -14,8 +16,8 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-    
-
+      
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
