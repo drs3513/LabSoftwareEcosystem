8c2b0e4e053f839ef473673944f2aeea4f5b7507
drs3513 <159196477+drs3513@users.noreply.github.com>
2025-03-10 14:27:43 -0500

    Fixing deployment

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,13 +1,2 @@
 import { defineAuth } from "@aws-amplify/backend";
 
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-  userAttributes: {
-    preferredUsername: {
-      mutable: true,
-      required: true,
-    },
-  },
-});
